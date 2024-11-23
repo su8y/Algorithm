1065cff242e05b6f12890a42ce039baf855dbbfd
Sua Bae <bsa0530@gmail.com>
2024-11-23 11:12:50 +0900

    add(boj): 25083

---
+++ backjoon/25083.java
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+#define ll long long;
+#define endl "\n";
+
+#ifdef BOJ
+#define debug(x)  ((void)0)
+#else
+#define debug(x) std::cout << "[Debug] " << #x <<" is " << x << '\n'
+#endif
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+    return 0;
+}
+
