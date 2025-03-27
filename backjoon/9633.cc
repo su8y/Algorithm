355dfe2b2e38427468f6eea4090fb584a5c5bf5a
Sua Bae <bsa0530@gmail.com>
2025-03-27 17:17:47 +0900

    add(boj): 9633

---
+++ backjoon/9633.cc
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, cnt = 0;
+
+// int map[15];
+vector<int> map;
+
+int check(int y){
+    for (size_t i = 0; i < y; i++)
+    {
+        if(map[y] == map[i] || y - i  == abs(map[y] - map[i]))
+        return 0;
+    }
+        return 1;
+   
+}
+
+void solve( int depth){
+    if(depth == n){
+        cnt = cnt + 1;
+        return;
+    }
+
+    for(int i = 0; i < n;i++){
+        map[depth] = i;
+        if(check(depth)){
+            solve(depth + 1);
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    clock_t f,s;
+    cin >> n; 
+    s = clock();
+    map.resize(n,-1);
+
+    solve(0);
+    cout << cnt << endl;;
+    f = clock();
+    cout << (f-s)/CLOCKS_PER_SEC;
+
+    return 0;
+}
