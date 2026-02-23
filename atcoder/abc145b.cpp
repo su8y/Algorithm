329a5843357b5863c3d29544e23e28554e74a823
Sua Bae <bsa0530@gmail.com>
2026-02-23 22:22:14 +0900

    [atcode] abc145b.cpp

---
+++ atcoder/abc145b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n; string s; cin >> n >> s;
+    if (n % 2 != 0) cout << "No" << endl;
+    else {
+        if (s.substr(0, n / 2) == s.substr(n / 2)) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+    return 0;
+}
